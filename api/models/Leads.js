3bfbc365f0eeddab9443a53f925d7605622427b4
unknown <wandersonwallison@gmail.com>
2018-05-25 15:55:24 -0300

    nova classe

---
+++ api/models/Leads.js
@@ -0,0 +1,42 @@
+/**
+ * Leads.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    email: { type: 'string', unique: true, allowNull: false },
+    nome: { type: 'string', allowNull: false },
+    endereco: { type: 'string', required: true },
+    bairro: { type: 'string', required: true },
+    cidade: { type: 'string', required: true },
+    telefone: { type: 'string', allowNull: true, defaultsTo: '11-22222-3333' },
+    data_nascimento: { type: 'number', required: true },
+    cpf: { type: 'string', required: true },
+    atividade: { type: 'string', required: true },
+    escolaridade: { type: 'string', required: true },
+    ativo: { type: 'boolean', defaultsTo: true }
+
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
